5adb0eb4107613a92f0fee0ea235acf14e9c8bf2
Kirill <kirill-22ru@yandex.ru>
2023-10-26 23:23:10 +0300

    Done task 4

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 #include "tree.h"
 
 int main(){
-    tree tr;
-    
+    //Среднее арифметическое
+    tree tr1;
     for(int i = 1; i < 16; i++){
-        tr.add_item(i);
+        tr1.add_item(i);}
+    tr1.print_brach_mean();
+    //Подсчёт по значению
+    tree tr2;
+    std::cout << "Записано в дерево: ";
+    for(int i = 1; i < 25; i++){
+        tr2.add_item(i%5);
+        std::cout << i%5 << " ";
     }
-    tr.print_brach_mean();
-    std::cout << tr.count_elements(3) << std::endl;
+    std::cout << std::endl << "Количество элементов со значением 3 равно " << tr2.count_elements(3) << std::endl;
     return 0;
+    //Удаление происходит автоматически
 }
--- tree.h
+++ tree.h
@@ -5,11 +5,14 @@
 #include <queue>
 
 class tree{
+    //Структура узла дерева
     struct tree_item{
         tree_item* left;
         tree_item* right;
         double value;
+        //Метод, считающий сумму узлов дерева и их количество
         void branch_sum(double&, int&);
+        //Метод, считающий количество узлов, значение в которых совпадает со значением параметра
         int count_elements(double);
         tree_item(double);
         ~tree_item();
@@ -17,8 +20,11 @@ class tree{
     int records;
     tree_item* root;
     public:
+    //Метод, считающий средние арифметические поддеревьев и выводящий результат в консоль
     void print_brach_mean();
+    //Метод, считающий количество узлов, значение в которых совпадает со значением параметра и возвращающий результат
     int count_elements(double);
+    //Метод, добавляющий новый узел в дерево
     void add_item(double);
     tree();
     ~tree();
